b97bf51fb920c5c5121264c594be4d14aded79e1
Zoe13Git <zoe13.mail@gmail.com>
2022-12-09 15:33:37 +0000

    add task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+
+int main () {
+	char ch;
+	
+	for(ch = 'a' ; ch <= 'z' ; ch++) 
+	{
+		putchar(ch);
+	}
+	
+	return(0);
+}
