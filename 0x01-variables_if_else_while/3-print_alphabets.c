6a4627cc8808541ed8aa90d90eda4aeb865e4789
Zoe13Git <zoe13.mail@gmail.com>
2022-12-09 15:57:05 +0000

    update task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a' ; ch <= 'z' ; ch++) 
+	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		putchar(ch);
 	}
@@ -18,5 +18,5 @@ int main(void)
 	}
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
