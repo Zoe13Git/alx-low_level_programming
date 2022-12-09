ccfe78c49768bfe521a97c05f92bc2026e01921b
Zoe13Git <zoe13.mail@gmail.com>
2022-12-09 16:25:29 +0000

    add task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 48 ; ch <= 57 ; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
