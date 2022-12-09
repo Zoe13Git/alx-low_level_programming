e62c2f4ebb7641c76e146dc0b90766b047cdea25
Zoe13Git <zoe13.mail@gmail.com>
2022-12-09 02:22:10 +0000

    update task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 /**
- * main - function
+ * main - Entry point
+ *
  * Return: 0 when completed
  */
-
 int main(void)
 {
-	printf("%s", "with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
