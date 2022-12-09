e22d546568317970a1495c8e0d7be063b4414493
Zoe13Git <zoe13.mail@gmail.com>
2022-12-09 02:43:21 +0000

    add task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,14 +6,17 @@
 int main(void)
 {
 	int num1;
-	float num2;
-	double num3;
+	long int num2;
+	long long int num3;
+	float num4;
 	char letter;
 
-	printf("Size of int: %ld bytes\n", sizeof(num1));
-	printf("Size of float: %ld bytes\n", sizeof(num2));
-	printf("Size of double: %ld bytes\n", sizeof(num3));
-	printf("Size of char: %ld byte\n", sizeof(letter));
+	printf("Size of char: %ld byte(s)\n", sizeof(letter));
+	printf("Size of int: %ld bytes(s)\n", sizeof(num1));	
+	printf("Size of long int: %ld bytes(s)\n", sizeof(num2));
+	printf("Size of long long int: %ld byte(s)\n", sizeof(num3));
+	printf("Size of float: %ld byte(s)\n", sizeof(num4));
+
 	return (0);
 }
 
