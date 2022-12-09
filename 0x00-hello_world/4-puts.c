b25e63a6c2160ec5ad89162720cb2e50c462e115
Zoe13Git <zoe13.mail@gmail.com>
2022-12-09 02:04:40 +0000

    update task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 /**
- * main 
+ * main - this is the main function
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 };
 
