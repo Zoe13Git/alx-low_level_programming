3c080e7e7016ac2044db8f6272469063759823f3
Zoe13Git <zoe13.mail@gmail.com>
2022-12-13 18:45:32 +0000

    Add task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Write a program that prints _putchar, followed by a new line.
+ * The program should return 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("_putchar");
+
+	return (0);
+}
