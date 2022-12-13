e79adb35117a91a79c2e3dfdc5f77fb545c4116f
Zoe13Git <zoe13.mail@gmail.com>
2022-12-13 20:32:29 +0000

    add task1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,17 @@
  * Write a program that prints _putchar, followed by a new line.
  * The program should return 0
  */
+
+void print_alphabet(void);
+
 puts("_putchar");
+
+void print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+}
