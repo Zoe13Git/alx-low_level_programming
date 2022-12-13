7fb0479ef004b90401d80779d618842a88e5e0a1
Zoe13Git <zoe13.mail@gmail.com>
2022-12-13 06:36:59 +0000

    update task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,7 @@
  * combinations of three digits.
  * - Numbers must be separated by , followed by a space
  * - The three digits must be different
- * - 012, 120, 102, 021, 201, 210 are considered the 
+ * - 012, 120, 102, 021, 201, 210 are considered the
  *   same combination of the three digits 0, 1 and 2
  * - Print only the smallest combination of two digits
  * - Numbers should be printed in ascending order, with
