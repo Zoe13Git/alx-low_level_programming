b83e76f5c4416686916ded7f480501d3e44cfca6
Zoe13Git <zoe13.mail@gmail.com>
2022-12-13 06:14:25 +0000

    update task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,9 @@
  * - 01 and 10 are considered the same combination of
  *   the two digits 0 and 1
  * - Print only the smallest combination of two digits
- * - Numbers should be printed in ascending order, with 
+ * - Numbers should be printed in ascending order, with
  *   two digits
- * - You can only use the putchar function 
+ * - You can only use the putchar function
  *   (every other function (printf, puts, etc…) is forbidden)
  * - You can only use putchar five times maximum in your code
  * - You are not allowed to use any variable of type char
@@ -19,14 +19,14 @@
  * Return: Always 0
  */
 
-int main (void)
+int main(void)
 {
 	int ch;
 	int ch2;
 
 	for (ch = 48; ch <= 57; ch++)
 	{
-		for (ch2 = 48; ch2 <=57; ch2++)
+		for (ch2 = 48; ch2 <= 57; ch2++)
 		{
 			if (ch == ch2 || ch2 < ch)
 				continue;
