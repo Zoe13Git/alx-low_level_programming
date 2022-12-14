ea3370d505c625e03fe0f952c0cab7e0a524c4bc
Zoe13Git <zoe13.mail@gmail.com>
2022-12-14 13:13:37 +0000

    Update task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,11 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (i * j < 10)
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (i * j < 10)
 			{
 				_putchar(' ');
 				_putchar('0' + (i * j));
