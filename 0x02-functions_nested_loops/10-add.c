d7a4e1a593c9181c4de87c439e0948b7b6e038a2
Zoe13Git <zoe13.mail@gmail.com>
2022-12-14 13:19:39 +0000

    Update task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
  * add - returns the sum of two numbers
  * @num1: First number to sum
  * @num2: Second number to sum
- * Return: num1 + num2. 
+ * Return: num1 + num2.
  */
 int add(int num1, int num2)
 {
