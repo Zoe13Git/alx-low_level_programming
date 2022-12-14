72df8b545c5faf13575e3832a6cd161169249b5f
Zoe13Git <zoe13.mail@gmail.com>
2022-12-14 19:46:54 +0000

    Add task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table
+ * starts from 0.
+ * Return: Always 0 (success).
+ */
+void print_times_table(int n)
+{
+	if (n >= 0 || n <= 15) 
+	{
+		int i;
+		int j;
+
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				if (j == 0)
+				{
+					_putchar('0');
+				}
+				else if (i * j < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (i * j));
+				}
+				else if (i * j < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (i * j) / 10);
+					_putchar('0' + (i * j) % 10);
+				}
+				else
+				{
+					_putchar('0' + (i * j) / 100);
+					_putchar('0' + (i * j) / 10 % 10);
+					_putchar('0' + (i * j) % 10);
+				}
+
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
