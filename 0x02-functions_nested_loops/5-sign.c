20a826164e00c67c28e2089b0645beb7dbaa8498
Zoe13Git <zoe13.mail@gmail.com>
2022-12-14 11:37:12 +0000

    Update task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,23 +2,23 @@
 
 /**
  * print_sign - prints the sign of an integer
- * @n integer to check
+ * @n: The integer to check
  * Return: 1 if n is greater than zero
  * 0 if n is zero and -1 if n is less than zero
  */
 int print_sign(int n)
 {
-	if (n > 0) 
-	{	
+	if (n > 0)
+	{
 		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
-	{	
-		_putchar(' ');
+	{
+		_putchar('0');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);
