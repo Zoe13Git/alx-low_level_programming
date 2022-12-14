e6b68e5e222cae85744667e89babb3f1ea5b9d56
Zoe13Git <zoe13.mail@gmail.com>
2022-12-14 19:24:47 +0000

    Update task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,6 +11,7 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		int i = 0;
+
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
@@ -22,6 +23,7 @@ void print_to_98(int n)
 	else
 	{
 		int i = 0;
+
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
