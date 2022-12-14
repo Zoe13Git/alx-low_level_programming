a0225e45cd515b103983e8183ff174916d59dbc2
Zoe13Git <zoe13.mail@gmail.com>
2022-12-14 11:33:28 +0000

    Update task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,16 +6,21 @@
  * Return: 1 if n is greater than zero
  * 0 if n is zero and -1 if n is less than zero
  */
-
 int print_sign(int n)
 {
 	if (n > 0) 
+	{	
 		_putchar('+');
 		return (1);
+	}
 	else if (n == 0)
+	{	
 		_putchar(' ');
 		return (0);
+	}
 	else 
+	{
 		_putchar('-');
 		return (-1);
+	}
 }
