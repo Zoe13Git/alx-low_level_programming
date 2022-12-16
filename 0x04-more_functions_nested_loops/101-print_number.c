e6ebd61b0dea0d61e30b30ea0e3629ce6f296f9e
Zoe13Git <zoe13.mail@gmail.com>
2022-12-16 12:37:52 +0000

    Add task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: Integer entered
+ * Return: Always 0 (success).
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	putchar('0' + n % 10);
+}
