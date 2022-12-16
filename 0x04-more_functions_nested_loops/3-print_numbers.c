5a3280e7927843a0a7f5b9ad72b9b8d725261bbb
Zoe13Git <zoe13.mail@gmail.com>
2022-12-16 01:39:03 +0000

    Add task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 1 to 9
+ * Return: Always 0 (success)
+ */
+void print_numbers(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		_putchar(c);
+	_putchar('\n');
+}
