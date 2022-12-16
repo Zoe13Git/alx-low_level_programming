3fea4e8dc98954c975045fd73c6efb6fc147865a
Zoe13Git <zoe13.mail@gmail.com>
2022-12-16 02:55:58 +0000

    Add task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,14 +9,13 @@ void print_diagonal(int n)
 {
 	if (n > 0)
 	{
-		for (int i = 0; i < n; i++)
+		for (int i = 1; i <= n; i++)
 		{
-			for (int j = 0; j < i; j++)
+			for (int j = 1; j < i; j++)
 				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 	else
 	{
