49aed5a94731abfea822dd57295a8a70ab2e68fb
Zoe13Git <zoe13.mail@gmail.com>
2022-12-20 18:34:10 +0000

    Add task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: String
+ * Return: Always (0);
+ */
+void _puts(char *str)
+{
+	int c = 0;
+
+	while (*(str + c))
+		putchar(*(str + c++));
+	putchar('\n');
+}
