ed2f3b6d107e82f88da0b98e9ae535d60198100a
Zoe13Git <zoe13.mail@gmail.com>
2022-12-20 13:58:40 +0000

    Add task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Takes a pointer to an int and updates 
+ * the values it points to to 98.
+ * @n: Pointer of an int
+ * Return: Always (0);
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
