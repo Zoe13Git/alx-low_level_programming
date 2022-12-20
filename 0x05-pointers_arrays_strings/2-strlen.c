75f249b44dbde2eef19125ac5d0b9ff80caceaf7
Zoe13Git <zoe13.mail@gmail.com>
2022-12-20 18:11:19 +0000

    Add task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - returns length of string.
+ * @s: String
+ * Return: int length of string
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+	int len = 0;
+
+	while (*(s + count))
+		len++
+
+	return (len);
+}
