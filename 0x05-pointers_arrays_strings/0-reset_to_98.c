c09f667a447f18e59872f5a2804b5234508cd551
Zoe13Git <zoe13.mail@gmail.com>
2022-12-20 18:03:39 +0000

    Update task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 /**
- * reset_to_98 - Takes a pointer to an int and updates 
+ * reset_to_98 - Takes a pointer to an int and updates
  * the values it points to to 98.
  * @n: Pointer of an int
  * Return: Always (0);
