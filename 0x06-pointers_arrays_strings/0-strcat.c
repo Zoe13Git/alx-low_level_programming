f50071a597df2714767c9fe0ece451ef00c20acb
Zoe13Git <zoe13.mail@gmail.com>
2022-12-22 02:27:08 +0000

    Add task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: Beginning string
+ * @src: String to concate
+ * Return: A character
+ */
+char *_strcat(char *dest, char *src)
+{
+	size_t dest_len = strlen(dest);
+	size_t i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
