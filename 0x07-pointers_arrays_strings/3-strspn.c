28bae0994b75b81271a9dedf3cc5264e6e29cfa7
Zoe13Git <zoe13.mail@gmail.com>
2023-01-03 10:41:58 +0000

    Update task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _strspn - Returns the number of bytes in the initial 
+ * _strspn - Returns the number of bytes in the initial
  * segment of s which consist only of bytes from accept
  * @s: Pointer. null-terminated string to be scanned
- * @accept: Pointer. null-terminated string containing 
-   the characters to match.
+ * @accept: Pointer. null-terminated string containing
+ * the characters to match.
  * Return: returns the length of the segment.
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int len =0;
+	unsigned int len = 0;
 
-	if((s == NULL) || (accept == NULL))
-		return len;
-	while(*s && strchr(accept,*s++))
-	{
-	len++;
-	}
-	return len;
+	if ((s == NULL) || (accept == NULL))
+		return (len);
+	while (*s && strchr(accept, *s++))
+		len++;
+
+	return (len);
 }
