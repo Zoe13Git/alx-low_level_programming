75e7c82cf165d21d53fd81c317db45572ae079fd
Zoe13Git <zoe13.mail@gmail.com>
2023-01-03 08:47:13 +0000

    Add task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * *_memcpy - coies n bytes from memory area src to memory
+ * area dsc.
+ * @dest: where to copy the memory to
+ * @src: the memory area to copy from
+ * @n: the number of bytes
+ * Return: Pointer of type pointer.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+}
