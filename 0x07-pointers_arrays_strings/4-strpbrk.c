6fa52017b59785948117337a541c5cdf808b7afe
Zoe13Git <zoe13.mail@gmail.com>
2023-01-04 01:41:15 +0000

    Add task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * *_strpbrk - locate the first occurrence in the string
+ * s of any of the bytes in the string accept
+ * @s: string to be scanned
+ * @accept: string containing the characters to match
+ * Return: a pointer to the byte in s that matches one of
+ * the bytes in accept, or NULL if no such byte is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	if ((s == NULL) || (accept == NULL))
+		return (NULL);
+	while (*s)
+	{
+		if (strchr(accept, *s))
+			return (s);
+		else
+			s++;
+	}
+	return (NULL);
+}
