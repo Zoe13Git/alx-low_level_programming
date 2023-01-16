82641403f6202c1f4d2ff262e50887d637554611
Zoe13Git <zoe13.mail@gmail.com>
2023-01-16 21:39:29 +0000

    Add task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef MY_H
+#define MY_H
+/**
+ * struct dog - defines a new type struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
