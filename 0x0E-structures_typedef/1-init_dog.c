cde4be5da7f70c0d6ed67f3e750259a7e1adb023
Zoe13Git <zoe13.mail@gmail.com>
2023-01-16 21:48:49 +0000

    Add task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+
+/**
+ * init_dog - init struct dog
+ * @d: instance of struct dog
+ * @name: dog name to init with
+ * @age: dog age to init with
+ * @owner: dog owner to init with
+ * Return: Always 0 (success)
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = d;
+	d->age = age;
+	d->owner = owner;
+}
