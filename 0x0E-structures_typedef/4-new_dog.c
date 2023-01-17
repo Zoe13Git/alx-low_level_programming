54b0fdf55b9bce10e4aa1587858e3b9372782d4b
Zoe13Git <zoe13.mail@gmail.com>
2023-01-17 19:17:01 +0000

    Update task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -5,16 +6,36 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: name of owner of dog
+ * Description: Keeping the values of name and owner to themselves
  * Return: dog created;
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t new_d;
-	dog_t *d = &new_d;
+	int name_len, own_len, i;
+	dog_t *d = malloc(sizeof(dog_t));
 
-	new_d.name = name;
-	new_d.age = age;
-	new_d.owner = owner;
+	name_len = own_len = 0;
+
+	/* transversing the memory their lenth */
+	while(name[name_len++]);
+	while(owner[own_len++]);
+
+	if (d == NULL)
+		return (NULL);
+
+	d->name = malloc(name_len * sizeof(d->name));
+	if (!d)
+		return (NULL);
+	for (i = 0; i < name_len; i++)
+		d->name[i] = name[i];
+
+	d->age = age;
+
+	d->owner = malloc(own_len * sizeof(d->owner));
+	if (d == NULL)
+		return (NULL);
+	for (i = 0; i < own_len; i++)
+		d->owner[i] = owner[i];
 
 	return (d);
 }
