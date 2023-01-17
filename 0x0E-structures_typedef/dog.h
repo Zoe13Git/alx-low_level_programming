405d88225628e9f0588991210681e92d1374564b
Zoe13Git <zoe13.mail@gmail.com>
2023-01-17 18:34:38 +0000

    Add dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,7 @@ typedef struct dog
 /* Additional Function Prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
