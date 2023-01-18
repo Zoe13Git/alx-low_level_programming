04868fd45015b68bd46b1a410af291648232b28b
Zoe13Git <zoe13.mail@gmail.com>
2023-01-18 13:51:26 -0800

    Update task 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,2 +1,9 @@
+#include <stddef.h>
+#ifndef FUNCPOINT_H
+#define FUNCPOINT_H
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
+
+
+#endif
