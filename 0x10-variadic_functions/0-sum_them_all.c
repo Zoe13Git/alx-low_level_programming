dd3c110212a9c72f4aab52c8fa54bd3860ed9d2c
Zoe13Git <zoe13.mail@gmail.com>
2023-01-20 01:30:11 +0000

    Update betty for task 1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,18 +6,18 @@
  * @n: first arg, an int
  * Return: sum of all integer arguments
  */
-int sum_them_all(const unsigned int n,...)
+int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i, sum;
 
-	va_start (ap, n);         /* Initialize the argument list. */
+	va_start(ap, n);         /* Initialize the argument list. */
 
 	sum = 0;
 	for (i = 0; i < n; i++)
-		sum += va_arg (ap, int);    /* Get the next argument value. */
+		sum += va_arg(ap, int);    /* Get the next argument value. */
 
-	va_end (ap);                  /* Clean up. */
+	va_end(ap);                  /* Clean up. */
 
-	return sum;
+	return (sum);
 }
