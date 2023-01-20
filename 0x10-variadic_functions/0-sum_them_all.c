434d75045d02681e713ba069b505610eea6780d6
Zoe13Git <zoe13.mail@gmail.com>
2023-01-20 01:23:18 +0000

    Add task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - variadic function to sum all args
+ * @n: first arg, an int
+ * Return: sum of all integer arguments
+ */
+int sum_them_all(const unsigned int n,...)
+{
+	va_list ap;
+	unsigned int i, sum;
+
+	va_start (ap, n);         /* Initialize the argument list. */
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg (ap, int);    /* Get the next argument value. */
+
+	va_end (ap);                  /* Clean up. */
+
+	return sum;
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,5 @@
+#ifndef MY_H
+#define MY_H
+int sum_them_all(const unsigned int n, ...);
+
+#endif
