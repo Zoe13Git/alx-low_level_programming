6df6f3086469d4bc2cc5649c4854e82cdb43bc34
Zoe13Git <zoe13.mail@gmail.com>
2023-01-20 02:52:05 +0000

    Add task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,58 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints all args in a variadic function
+ * @format: c: char, i: integer, f: float, s: char *
+ * any other char should be ignored
+ * Return: 0 success
+ */
+void print_all(const char *format, ...)
+{
+	va_list ap;
+	int i = 0;
+	int valid_num = 0;
+
+	va_start (ap, format);
+	
+	while(format)
+	{
+		switch(format[i++])
+		{
+			case 'c':
+			case 'i':
+			case 'f':
+			case 's':
+				valid_num++;
+			default: continue;
+		}
+	}
+	printf("Valid Count = %d", valid_num);
+
+	i = 0;
+	while(format)
+	{
+		switch(format[i++])
+		{
+		case 'c': printf("%c ", va_arg(ap, int));
+			break;
+		case 'i': printf("%d ", va_arg(ap, int));
+			break;
+		case 'f': printf("%f ", va_arg(ap, double));
+			break;
+		case 's': 
+			{
+				char *str = va_arg(ap, char*);
+
+				if (str)
+					printf("%s", str);
+				else
+					printf("(nil)");
+				break;
+			}
+		default: 
+			continue;
+
+		}
+	}
+}
