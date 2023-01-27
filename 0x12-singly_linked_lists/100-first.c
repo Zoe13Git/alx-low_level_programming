379043e0b1be05d41f151b9326f7c49552f74d51
Zoe13Git <zoe13.mail@gmail.com>
2023-01-27 09:03:02 +0000

    Add task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * before_main - Construtor
+ * Description: Executes before the main function
+ * Return: void
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
+
