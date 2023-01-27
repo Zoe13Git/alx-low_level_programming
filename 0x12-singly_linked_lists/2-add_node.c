0052ab11d3c570b65dfb8a065b2edf143fa2f17b
Zoe13Git <zoe13.mail@gmail.com>
2023-01-27 07:49:18 +0000

    Update task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,13 +11,12 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
-	char *n_str = strdup(str);
 
 	if (new == NULL)
 		return (NULL);
 
-	new->str = n_str;
-	new->len = strlen(n_str);
+	new->str = strdup(str);
+	new->len = strlen(str);
 	new->next = *head;
 
 	*head = new;
