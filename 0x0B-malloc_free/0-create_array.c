c26d23273556a6f1cbaf92ae060d332b32cdfb68
Zoe13Git <zoe13.mail@gmail.com>
2023-01-29 18:51:30 +0000

    Update task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
 /**
  * create_array - creates an array of chars
  * and initialize it with specific char
- * @size:
+ * @size: size of array
  * @c: character
  * Return: A pointer to the array or NULL if it fails
  */
@@ -12,6 +12,9 @@ char *create_array(unsigned int size, char c)
 {
 	char *arr = malloc(sizeof(char) * size);
 	unsigned int i = 0;
+
+	if (!arr)
+		return (NULL);
 
 	while (i < size)
 		arr[i++] = c;
