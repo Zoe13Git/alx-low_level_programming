431703fbfbab8c3ceeb2209fc40436a20423fbf9
Zoe13Git <zoe13.mail@gmail.com>
2023-03-16 09:16:55 +0000

    fix bettycodestyle *head

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,19 +4,19 @@
  * add_dnodeint - add new node at the begginning of dlistint_t list
  * @head: pointer to first node
  * @n: value for new node
- * Return: pointer to new node
+ * Return: pointer to new nod or NULL if failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
-	
+
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
-		return NULL;
+		return (NULL);
 
 	new->n = n;
 	new->prev = NULL;
-	if (head)
+	if (*head)
 		new->next = *head;
 	else
 		new->next = NULL;
