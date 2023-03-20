1c54d06b3a3c3869613fef8fe4a7f7b80e9bbb83
Zoe13Git <zoe13.mail@gmail.com>
2023-03-20 09:57:24 +0000

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,13 @@
 void print_rev(char *s)
 {
 	int i, len = 0, c = 0;
+
 	while (*(s + c++))
 	{
 		len++;
 	}
 
-	for (i = len; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 		_putchar(*(s + i));
 	_putchar('\n');
 }
