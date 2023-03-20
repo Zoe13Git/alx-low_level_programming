8770be0235c5ea4f25d9d53e102b6f997d360a14
Zoe13Git <zoe13.mail@gmail.com>
2023-03-20 11:27:03 +0000

    add

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an arr
+ * @a: array
+ * @n: number of elemenst to print
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%d\n", a[i]);
+			continue;
+		}
+		printf("%d, ", a[i]);
+	}
+}
