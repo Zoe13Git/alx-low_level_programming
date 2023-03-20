37f08b3121b5cf6b1d5722a8ae88681e4197d175
Zoe13Git <zoe13.mail@gmail.com>
2023-03-20 11:06:57 +0000

    update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,10 +12,7 @@ void puts_half(char *str)
 	for (len = 0; str[len] != '\0'; ++len)
 		;
 
-	if (len % 2 == 0)
-		n = (len) / 2;
-	else
-		n = (len - 1) / 2;
+	n = (len - 1) / 2;
 
 	for (i = n; i < len; i++)
 		_putchar(*(str + i));
