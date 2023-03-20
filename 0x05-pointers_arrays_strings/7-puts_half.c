cc15bd9cc982b574bcc9fea177785e01bbea0350
Zoe13Git <zoe13.mail@gmail.com>
2023-03-20 11:04:54 +0000

    add

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, n, len = 0;
+
+	for (len = 0; str[len] != '\0'; ++len)
+		;
+
+	if (len % 2 == 0)
+		n = (len) / 2;
+	else
+		n = (len - 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(*(str + i));
+	_putchar('\n');
+}
