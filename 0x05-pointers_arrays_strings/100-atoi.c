beb3f6ed67169c41d5f3b79f526538bc2b605739
Zoe13Git <zoe13.mail@gmail.com>
2023-03-20 12:45:49 +0000

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,14 @@
 int _atoi(char *s)
 {
 	int num = 0, i, sign = 1;
-	int chk = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] == '-')
+			sign *= -1;
+
 		if (s[i] >= 48 && s[i] <= 57)
 		{
-			if (chk == 0)
-			{
-				chk = 1;
-				if (i != 0)
-				{
-					if (s[i - 1] == '-')
-						sign = -1;
-				}
-			}
-
 			num = num * 10 + (s[i] - '0');
 
 			if (s[i + 1] != '\0')
