99bc2e92bb6f480d2802096419e834762e56a441
Zoe13Git <zoe13.mail@gmail.com>
2023-03-22 12:31:57 +0000

    change index check to idx=++index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,7 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	tmp = *h;
 	while (tmp->next)
 	{
-		if (idx == index++)
+		if (idx == ++index)
 		{
 			new->prev = tmp;
 			new->next = tmp->next;
