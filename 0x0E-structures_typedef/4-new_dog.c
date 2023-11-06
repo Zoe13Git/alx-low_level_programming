554e51156b8448c8982f64746e8177c85682aa09
Zoe13Git <zoe13.mail@gmail.com>
2023-11-06 21:33:13 +0100

    update task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,12 +19,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name)
 	{
-		while (name[len] != '\0')
-			len++;
-		new->name = malloc(sizeof(char) * len + 1);
+		while (name[len++])
+			;
+		new->name = malloc(sizeof(new->name) * len);
+		if (!new->name)
+			return (NULL);
 		for (i = 0; i < len; i++)
 			new->name[i] = name[i];
-		new->name[i] = '\0';
 	}
 
 	new->age = age;
@@ -34,6 +35,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		while (owner[len2] != '\0')
 			len2++;
 		new->owner = malloc(sizeof(char) * len2 + 1);
+		if (!new->owner)
+			return (NULL);
 		for (i = 0; i < len2; i++)
 			new->owner[i] = owner[i];
 		new->owner[i] = '\0';
