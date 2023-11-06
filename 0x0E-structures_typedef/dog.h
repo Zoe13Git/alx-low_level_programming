8be205835999c25d33d54442f84101b1c184eadb
Zoe13Git <zoe13.mail@gmail.com>
2023-11-06 17:40:49 +0100

    add task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for dog entity
+ * @name: string, name of dog
+ * @age: float, age of dog
+ * @owner: string, owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
