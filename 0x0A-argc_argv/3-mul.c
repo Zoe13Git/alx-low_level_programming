c8b357ff0bf8dcbbf19e77eb755503ea9fd8d91f
Zoe13Git <zoe13.mail@gmail.com>
2023-11-07 12:12:11 +0100

    add task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int _atoi(char *s);
+
+/**
+ * main - multiply 2 numbers
+ * @argc: number of arguments
+ * @argv: array of arguments passed
+ * Return: 1 on success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+		printf("Erorr\n");
+	else
+		printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
+	return (0);
+}
+
+/**
+ * _atoi - converts a string int to int
+ * @s: string int
+ * Return: int of string int
+ */
+int _atoi(char *s)
+{
+	int res = 0, sign = 1, i = 0;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i = 1;
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] > '9' || s[i] < '0')
+			return (-1);
+		res = res * 10 + s[i] - '0';
+		i++;
+	}
+
+	return (res * sign);
+}
