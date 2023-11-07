0bf6c6f66fcfd9e0b7600c78c3f2734d74647801
Zoe13Git <zoe13.mail@gmail.com>
2023-11-07 14:44:20 +0100

    add task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+	long int fib1 = 1;
+	long int fib2 = 2;
+	int i = 0;
+
+	for (i = 0; i < 98; i++)
+	{
+		long int temp = fib2;
+
+		if (i == 97)
+		{
+			printf("%ld, %ld\n", fib1, fib2);
+			continue;
+		}
+		printf("%ld, %ld, ", fib1, fib2);
+		fib1 = fib1 + fib2;
+		fib2 = fib1 + temp;
+	}
+
+	return (0);
+}
