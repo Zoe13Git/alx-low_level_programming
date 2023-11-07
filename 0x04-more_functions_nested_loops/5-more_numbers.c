a9b4e478aa5d44f29bc903ed433b5ead6b455791
Zoe13Git <zoe13.mail@gmail.com>
2023-11-07 14:18:14 +0100

    update task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * more_numbers - prints 0 to 14, 10 times
@@ -10,13 +11,13 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		char c;
+		char c, one = '1';
 
 		for (c = '0'; c <= '9'; c++)
 			_putchar(c);
 		for (c = '0'; c <= '4'; c++)
 		{
-			_putchar('1');
+			(write(1, &one, 1));
 			_putchar(c);
 		}
 		_putchar('\n');
