b882e747f9e8c0adc0babb12eca067a52c737141
Zoe13Git <zoe13.mail@gmail.com>
2023-11-07 13:51:50 +0100

    update task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,13 +10,14 @@
 int main(int argc, char *argv[])
 {
 	int coins[] = {25, 10, 5, 2, 1};
-	int i, count = 0, amount = atoi(argv[1]);
+	int i, count = 0, amount;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	amount = atoi(argv[1]);
 	for (i = 0; i < 5; i++)
 	{
 		while (amount >= coins[i])
